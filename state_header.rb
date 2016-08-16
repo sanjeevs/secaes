000eb879a80590ce07b7c8cbb031b374bc622c65
sanjeev singh <snjvsingh123@gmail.com>
2016-08-16 19:06:47 -0400

    added ctrl byte

--- state_header.rb
+++ state_header.rb
@@ -5,13 +5,30 @@ class StateHeader
     @no_ciph = args[:no_ciph] || 0
     @icv_size = args[:icv_size] || 16
     @auth_only = args[:auth_only] || 0
-    @key256 = args[:key256] || 0
+    @key256 = args[:key256] || 1
     @use_esn = args[:use_esn] || 0
     @sn = args[:sn] || 0
     @spi = args[:spi] || 0 
     @salt = args[:salt] || 0 
     @iv = args[:iv] || 0
     @key = args[:key] || 0
+
+    unless [8, 12, 16].include? icv_size
+      raise ArgumentError, "Invalid size of icv. Valid values are 8,12,16. Got #{icv_size}"
+    end
+
+  end
+
+  def icv_size_enc
+    return 0 if icv_size == 16
+    return 1 if icv_size == 12
+    return 2 if icv_size == 8
+    raise ArgumentError, "Invalid valie of icv size"
+  end
+
+  def ctrl_byte
+    skip_replay = 1
+    skip_replay << 6 | no_ciph << 5 | icv_size_enc << 3 | auth_only << 2 | key256 << 1 | use_esn
   end
 
   def to_json
@@ -33,8 +50,10 @@ class StateHeader
   end
 
   def value
-   "0x00000000000000" + sn.to_s(16).rjust(16, '0') +
-    spi.to_s(16).rjust(8, '0') +  # CTRL is skipped. FIXME
+   "0x00000000000000" + 
+    sn.to_s(16).rjust(16, '0') +
+    spi.to_s(16).rjust(8, '0') + 
+    ctrl_byte.to_s(16).rjust(8, '0') +
     salt.to_s(16).rjust(8, '0') +
     iv.to_s(16).rjust(16, '0') +
     key.to_s(16).rjust(64, '0')
--- test_state_header.rb
+++ test_state_header.rb
@@ -5,19 +5,20 @@ require 'json'
 class TestStateHeader < MiniTest::Unit::TestCase
   def setup
     @state_header = StateHeader.new(sn: 0x01234567)
-    @json_hash = JSON.parse(@state_header.to_json)
-    p @json_hash
+  end
+
+  def test_ctrl_byte
+    assert_equal(66, @state_header.ctrl_byte)
   end
 
   def test_sn
     assert_equal(0x01234567, @state_header.sn)
   end
 
-  def test_json_sn
-    assert_equal("0x1234567", @json_hash["sn"])
-  end
-  
-  def test_json_default_spi
-    assert_equal("0x0", @json_hash["spi"])
+  def test_json
+    json_hash = JSON.parse(@state_header.to_json)
+    p json_hash
+    assert_equal("0x1234567", json_hash["sn"])
+    assert_equal("0x0", json_hash["spi"])
   end
 end
